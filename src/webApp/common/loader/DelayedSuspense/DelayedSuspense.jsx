9ffd311ca6e0d28c9c060a997e86890f350e86a3
yash-babariya <yashbabariya549@gmail.com>
2024-08-31 12:11:46 +0530

    some change

--- src/webApp/common/loader/DelayedSuspense/DelayedSuspense.jsx
+++ src/webApp/common/loader/DelayedSuspense/DelayedSuspense.jsx
@@ -1,22 +1,59 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
+// import Loader from '..';
+
+// const DelayedSuspense = ({ children, delay = 2000 }) => {
+//     const [isLoaded, setIsLoaded] = useState(false);
+
+//     useEffect(() => {
+//         const timer = setTimeout(() => setIsLoaded(true), delay);
+//         return () => clearTimeout(timer);
+//     }, [delay]);
+
+//     return isLoaded ? children : <Loader />;
+// };
+
+// export default DelayedSuspense;
+
+
+// import React, { useEffect, useState } from 'react';
+// import Loader from '..';
+
+// const DelayedSuspense = ({ children, delay = 2000 }) => {
+//     const [isLoaded, setIsLoaded] = useState(false);
+
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             await new Promise(resolve => setTimeout(resolve, delay));
+//             setIsLoaded(true);
+//         };
+
+//         fetchData();
+
+//         // Clean-up function if needed
+//         return () => { };
+//     }, [delay]);
+
+//     return isLoaded ? children : <Loader />;
+// };
+
+// export default DelayedSuspense;
+
+
+import React, { useState } from 'react';
 import Loader from '..';
 
-const DelayedSuspense = ({ children, delay = 2000 }) => {
-    const [show, setShow] = useState(false);
+const DelayedSuspense = ({ children }) => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const handleLoad = () => {
+        setIsLoaded(true);
+    };
+    if (!isLoaded) {
+        setTimeout(handleLoad, 2000);
+    }
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setShow(true);
-        }, delay);
-
-        return () => clearTimeout(timer);
-    }, [delay]);
-
-    return (
-        <React.Suspense fallback={<Loader />}>
-            {show ? children : <Loader />}
-        </React.Suspense>
-    );
+    return isLoaded ? children : <Loader />;
 };
 
 export default DelayedSuspense;
+
+
